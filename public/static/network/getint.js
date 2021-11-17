e59768c9d5d56ebeee044a5f7e31c0d8344b1f87
ReturnTo ThisEra <65372786+CodeSalvageON@users.noreply.github.com>
2021-11-17 18:37:00 +0000

    Added a bit more to the networking scripts, including client checking for browser and app

--- public/static/network/getint.js
+++ public/static/network/getint.js
@@ -1,1 +1,74 @@
 // Ping the server every once in a while to find out if it is day or night 
+setInterval (function () { // Every one minute the server is pinged!
+  if (client_type === "browser") {
+    fetch ("/weather")
+    .then(response => response.text())
+    .then(data => {
+      if (data === "sunny") {
+        createDay("sunny");
+      }
+
+      else if (data === "cloudy") {
+        createDay("cloudy");
+      }
+
+      else if (data === "rain") {
+        createRain();
+      }
+
+      else if (data === "thunder") {
+        createThunder();
+      }
+
+      else if (data === "snowy") {}
+
+      else {
+        console.log("Connection to weather route on server lost.");
+      }
+    })
+    .catch(error => {
+      throw error;
+    });
+  }
+
+  else if (client === "app") {
+    
+  }
+
+  else {
+    console.log("Invalid client used.");
+
+    // Here there be dragons!
+  }
+}, 60000);
+
+setInterval(function () { // Every 5 minutes the server is pinged!
+  if (client_type === "browser") {
+    fetch ("/day")
+    .then(response => response.text())
+    .then(data => {
+      if (data === "day") {
+        stopThunder();
+        createDay("day");
+      }
+
+      else {
+        stopThunder();
+        createDay("night");
+      }
+    })
+    .catch(error => {
+      throw error;
+    });
+  }
+
+  else if (client_type === "app") {
+    
+  }
+
+  else {
+    console.log("Invalid client used.");
+
+    // Here there be dragons!
+  }
+}, 300000);
