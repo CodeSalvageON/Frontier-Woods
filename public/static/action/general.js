f418ed09734af1676c507580d8e3d4036f3ed7a2
ReturnTo ThisEra <65372786+CodeSalvageON@users.noreply.github.com>
2021-11-17 17:02:46 +0000

    Added JQuery library and began work on the game itself (survival script)

--- public/static/action/general.js
+++ public/static/action/general.js
@@ -1,4 +1,5 @@
 // General Script for listing off variables for DOM Manipulation
+// Begin
 
 // Body and sound effects
 const main_body = document.querySelector("body");
@@ -12,3 +13,7 @@ const hunger_stat = document.getElementById("hunger-stat");
 const hydro_stat = document.getElementById("hydro-stat");
 const exp_stat = document.getElementById("exp-stat");
 const knowledge_stat = document.getElementById("knowledge-stat");
+
+// Initial styling for the page, using JQuery
+$("#stats").hide();
+$("#stats2").hide();
---
+++ public/static/action/survival.js
@@ -0,0 +1,9 @@
+// Variables and functions for survival related things
+// Begin 
+
+let temp = 75;
+let health = 100;
+let hunger = 0;
+let hydro = 100;
+let exp = 0;
+let knowledge = 0;
